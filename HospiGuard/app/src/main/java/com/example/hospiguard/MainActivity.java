64aced00a38f87a88b7b6b9b8f6fa77e092c33fc
Denky <pedrojunqueira.ufg@gmail.com>
2024-01-29 05:34:50 -0300

    segunda tela

--- HospiGuard/app/src/main/java/com/example/hospiguard/MainActivity.java
+++ HospiGuard/app/src/main/java/com/example/hospiguard/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.hospiguard;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -18,17 +21,35 @@ import com.example.hospiguard.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView user, password;
+    private Button login;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        user = findViewById(R.id.edit_text_login);
+        password = findViewById(R.id.edit_text_password);
+        login = findViewById(R.id.btn_sign_in);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user.getText().toString().equals("adm") && password.getText().toString().equals("adm")) {
+                    Intent telaPatologias = new Intent( MainActivity.this, PatologyCheckboxActivity.class);
+                    startActivity(telaPatologias);
+                } else {
+                    showPasswordErrorDialog();
+                }
+            }
+        });
     }
 
     @Override
@@ -58,5 +79,22 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void showPasswordErrorDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Erro de Login");
+        builder.setMessage("A senha ou login inserido está incorreto. Tente novamente.");
+
+        // Adicionar botão OK para fechar o AlertDialog
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Fechar o AlertDialog
+                dialog.dismiss();
+            }
+        });
+        // Exibir o AlertDialog
+        builder.show();
     }
 }
---
+++ HospiGuard/app/src/main/java/com/example/hospiguard/PatologyCheckboxActivity.java
@@ -0,0 +1,50 @@
+package com.example.hospiguard;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class PatologyCheckboxActivity extends AppCompatActivity {
+    private Button save;
+    private ArrayList<String> patology;
+    private CheckBox checkbox1, checkbox2, checkbox3, checkbox4, checkbox5, checkbox6, checkbox7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patology_checkbox);
+
+        save = findViewById(R.id.btn_save);
+        checkbox1 = findViewById(R.id.checkbox1);
+        checkbox2 = findViewById(R.id.checkbox2);
+        checkbox3 = findViewById(R.id.checkbox3);
+        checkbox4 = findViewById(R.id.checkbox4);
+        checkbox5 = findViewById(R.id.checkbox5);
+        checkbox6 = findViewById(R.id.checkbox6);
+        checkbox7 = findViewById(R.id.checkbox7);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckBox[] checkboxes = {checkbox1, checkbox2, checkbox3, checkbox4, checkbox5, checkbox6, checkbox7};
+
+                for (CheckBox checkbox : checkboxes) {
+                    if (checkbox.isChecked()) {
+                        patology.add(checkbox.getText().toString());
+                    }
+                }
+//            Intent telaSensores = new Intent( PatologyCheckboxActivity.this, teste.class); *Matt* substitua "teste" pela sua tela com os sensores
+//            startActivity(telaSensores);
+            }
+        });
+    }
+}
