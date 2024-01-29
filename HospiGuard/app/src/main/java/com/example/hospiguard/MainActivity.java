2d42f2f5d2c151335d6ecf10cb6ff5d0a112aa7b
Matheus de Moura Rosa <matheusmoura@arcadium-studios.com>
2024-01-29 00:47:34 -0300

    Fixing some errors

--- HospiGuard/app/src/main/java/com/example/hospiguard/MainActivity.java
+++ HospiGuard/app/src/main/java/com/example/hospiguard/MainActivity.java
@@ -28,22 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Navigate to LightSensorFragment when FAB is clicked
-                navController.navigate(R.id.LightSensorFragment);
-            }
-        });
+        setContentView(R.layout.activity_main);
     }
 
     @Override
